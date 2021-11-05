348bab7f6caa695b8ddcb3a98d5b9d2e48d90a43
ntbloom <ntbloom@gmail.com>
2021-11-04 23:21:38 -0400

    using hook for last rain

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import UrlBuilder from '../lib/data/urlBuilder';
-import { LastRain } from './LastRain';
+import { LastRain, LastRainProps } from './LastRain';
 import '../styles/App.css';
 
 const urls = new UrlBuilder();
 
 function App() {
-  return <LastRain url={urls.lastRainURL}></LastRain>;
+  // return <LastRain url={urls.lastRainURL}></LastRain>;
+  const props: LastRainProps = {
+    url: urls.lastRainURL,
+  };
+  return LastRain(props);
 }
 
 export default App;
--- src/components/LastRain.tsx
+++ src/components/LastRain.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TimeUtils from '../lib/data/timeUtils';
 import UrlBuilder from '../lib/data/urlBuilder';
 
@@ -19,48 +19,67 @@ interface LastRainData {
   timestamp: string;
 }
 
-// LastRain shows the last time it rained and how many hours or days ago it was
-class LastRain extends React.Component<LastRainProps, LastRainState> {
-  constructor(props: LastRainProps) {
-    super(props);
-    this.state = {
-      date: '',
-      timeSince: '',
-      rawStamp: '',
-    };
-  }
+// // LastRain shows the last time it rained and how many hours or days ago it was
+// class LastRain extends React.Component<LastRainProps, LastRainState> {
+//   constructor(props: LastRainProps) {
+//     super(props);
+//     this.state = {
+//       date: '',
+//       timeSince: '',
+//       rawStamp: '',
+//     };
+//   }
 
-  // get last rain from API
-  apiCall() {
-    console.debug(`calling ${this.props.url}`);
-    UrlBuilder.apiCall(this.props.url).then((data) => {
-      const timestamp = (data as unknown as LastRainData).timestamp;
-      this.setState({
-        rawStamp: timestamp,
-        date: TimeUtils.getMonthDayYear(timestamp),
-        timeSince: `(${TimeUtils.getTimeSince(timestamp)} ago)`,
-      });
-    });
-  }
+//   // get last rain from API
+//   apiCall() {
+//     console.debug(`calling ${this.props.url}`);
+//     UrlBuilder.apiCall(this.props.url).then((data) => {
+//       const timestamp = (data as unknown as LastRainData).timestamp;
+//       this.setState({
+//         rawStamp: timestamp,
+//         date: TimeUtils.getMonthDayYear(timestamp),
+//         timeSince: `(${TimeUtils.getTimeSince(timestamp)} ago)`,
+//       });
+//     });
+//   }
 
-  componentDidMount() {
-    this.apiCall();
-  }
+//   componentDidMount() {
+//     this.apiCall();
+//   }
 
-  componentDidUpdate(_: LastRainProps, prevState: LastRainState) {
-    if (prevState.rawStamp !== this.state.rawStamp) {
-      this.apiCall();
-    }
-  }
+//   componentDidUpdate(_: LastRainProps, prevState: LastRainState) {
+//     if (prevState.rawStamp !== this.state.rawStamp) {
+//       this.apiCall();
+//     }
+//   }
 
-  render() {
-    return (
-      <p className="LastRain">
-        Last Rain Event: {this.state.date} {this.state.timeSince}
-      </p>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <p className="LastRain">
+//         Last Rain Event: {this.state.date} {this.state.timeSince}
+//       </p>
+//     );
+//   }
+// }
+// function LastRain() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//     </div>
+//   );
+// }
+
+const LastRain = (props: LastRainProps): JSX.Element => {
+  console.debug(`calling ${props.url}`);
+  const [date, setDate] = useState('');
+  UrlBuilder.apiCall(props.url).then((data) => {
+    const timestamp = (data as unknown as LastRainData).timestamp;
+    setDate(TimeUtils.getMonthDayYear(timestamp));
+  });
+  return <p className="LastRain">Last Rain Event: {date}</p>;
+};
 
 export type { LastRainProps };
 export { LastRain };
