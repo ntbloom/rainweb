e208a4d6f45592d1e714b86f6b92d72051ab3547
ntbloom <ntbloom@gmail.com>
2021-11-04 23:28:46 -0400

    cleaned up commented code

--- src/components/LastRain.tsx
+++ src/components/LastRain.tsx
@@ -8,77 +8,24 @@ interface LastRainProps {
   //   lastRainMM: number;
   //   lastRainDuration: number;
 }
-
-interface LastRainState {
-  date: string;
-  timeSince: string;
-  rawStamp: string;
-}
-
 interface LastRainData {
   timestamp: string;
 }
 
-// // LastRain shows the last time it rained and how many hours or days ago it was
-// class LastRain extends React.Component<LastRainProps, LastRainState> {
-//   constructor(props: LastRainProps) {
-//     super(props);
-//     this.state = {
-//       date: '',
-//       timeSince: '',
-//       rawStamp: '',
-//     };
-//   }
-
-//   // get last rain from API
-//   apiCall() {
-//     console.debug(`calling ${this.props.url}`);
-//     UrlBuilder.apiCall(this.props.url).then((data) => {
-//       const timestamp = (data as unknown as LastRainData).timestamp;
-//       this.setState({
-//         rawStamp: timestamp,
-//         date: TimeUtils.getMonthDayYear(timestamp),
-//         timeSince: `(${TimeUtils.getTimeSince(timestamp)} ago)`,
-//       });
-//     });
-//   }
-
-//   componentDidMount() {
-//     this.apiCall();
-//   }
-
-//   componentDidUpdate(_: LastRainProps, prevState: LastRainState) {
-//     if (prevState.rawStamp !== this.state.rawStamp) {
-//       this.apiCall();
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <p className="LastRain">
-//         Last Rain Event: {this.state.date} {this.state.timeSince}
-//       </p>
-//     );
-//   }
-// }
-// function LastRain() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>Click me</button>
-//     </div>
-//   );
-// }
-
 const LastRain = (props: LastRainProps): JSX.Element => {
   console.debug(`calling ${props.url}`);
   const [date, setDate] = useState('');
+  const [timeSince, setTimeSince] = useState('');
   UrlBuilder.apiCall(props.url).then((data) => {
     const timestamp = (data as unknown as LastRainData).timestamp;
     setDate(TimeUtils.getMonthDayYear(timestamp));
+    setTimeSince(`${TimeUtils.getTimeSince(timestamp)} ago`);
   });
-  return <p className="LastRain">Last Rain Event: {date}</p>;
+  return (
+    <p className="LastRain">
+      Last Rain Event: {date} ({timeSince})
+    </p>
+  );
 };
 
 export type { LastRainProps };
